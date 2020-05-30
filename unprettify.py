1a9e756dec31db2f3aec21eea4eafbcd988dca6a
commandblockguy <commandblockguy1@gmail.com>
2020-05-30 02:13:20 -0500

    Improve regexes

--- unprettify.py
+++ unprettify.py
@@ -6,8 +6,8 @@ import re
 with open(sys.argv[1],'rb') as ifile:
 	with open(sys.argv[2],'wb') as ofile:
 		contents = ifile.read()
-		contents = re.sub(b"[\\x11\\x09]+\\x3F", b"\x3F", contents) # Remove ) and } before a newline
-		contents = re.sub(b"[\\x11\\x09]+\\x3E", b"\x3E", contents) # Remove ) and } before a colon
-		contents = re.sub(b"\\x2A\\x3F", b"\x3F", contents) # Remove " before a newline
-		contents = re.sub(b"\\x29", b"", contents) # Remove spaces TODO: this breaks a few window and stats tokens, as well as expr(
+		contents = re.sub(b"[\\x11\\x09]+([\\x3F,\\x3E,\\x04])", b"\\1", contents) # Remove ) and } before a newline, colon, or arrow
+		contents = re.sub(b"\\x2A([\\x3F,\\x04])", b"\\1", contents) # Remove " before a newline or arrow
+		contents = re.sub(b"([^\\x5C-\\x5E\\x60-\\x63\\x7E\\xAA\\xBB\\xEF])\\x29+", b"\\1", contents) # Remove spaces
+		contents = re.sub(b"\\x3F+", b"\x3F", contents) # Remove empty lines
 		ofile.write(contents)
