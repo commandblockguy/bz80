f4746a813cc3bd84a260c5ab6ec930c0273bda14
commandblockguy <commandblockguy1@gmail.com>
2020-06-02 15:55:42 -0500

    Add indirect loading

--- include/mem.h
+++ include/mem.h
@@ -2,8 +2,8 @@
 #define H_MEM
 
 #define get_byte(address) mem(min(address+1,999))
-#define get2(address) mem(address+1) + 256 mem(address+2)
-#define get3(address) mem(address+1) + 256 mem(address+2) + 4^8 mem(address+3)
+#define get16(address) mem(address+1) + 256 mem(address+2)
+#define get24(address) mem(address+1) + 256 mem(address+2) + 4^8 mem(address+3)
 #define get_bytes(address, size) Σ(mem(address+X)256^(X-1), X, 1, size)
 #define set_byte(address, data) data→mem(address+1)
 #define set_bytes(address, size, data) For(temp_var, 1, size):remainder(int(data/256^temp_var),256)→mem(address+temp_var):End
