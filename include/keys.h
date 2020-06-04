ec3faf9d71a6c7e9c9c9450d78e6a442a73a2dab
commandblockguy <commandblockguy1@gmail.com>
2020-06-03 23:24:46 -0500

    Add debug options

---
+++ include/keys.h
@@ -0,0 +1,64 @@
+#ifndef H_KEYS
+#define H_KEYS
+
+#define key_yEqu    11
+#define key_window  12
+#define key_zoom    13
+#define key_trace   14
+#define key_group   15
+
+#define key_2nd     21
+#define key_mode    22
+#define key_del     23
+#define key_left    24
+#define key_up      25
+#define key_right   26
+
+#define key_alpha   31
+#define key_xton    32
+#define key_stat    33
+#define key_down    34
+
+#define key_math    41
+#define key_apps    42
+#define key_prgm    43
+#define key_vars    44
+#define key_clear   45
+
+#define key_recip   51
+#define key_sin     52
+#define key_cos     53
+#define key_tan     54
+#define key_exp     55
+
+#define key_squared 61
+#define key_comma   62
+#define key_lparen  63
+#define key_rparen  64
+#define key_divide  65
+
+#define key_log     71
+#define key_7       72
+#define key_8       73
+#define key_9       74
+#define key_product 75
+
+#define key_ln      81
+#define key_4       82
+#define key_5       83
+#define key_6       84
+#define key_minus   85
+
+#define key_store   91
+#define key_1       92
+#define key_2       93
+#define key_3       94
+#define key_plus    95
+
+#define key_on      101
+#define key_0       102
+#define key_period  103
+#define key_neg     104
+#define key_enter   105
+
+#endif
